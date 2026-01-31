286a61c097057ec24aad4a8040e95bd4cc58a554
LIKITH NAIDU <likith.anumakonda@gmail.com>
2026-01-31 17:03:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+// script.js
+
+// Full GitHub Pages URL of your PDF
+const pdfFile = 'https://likith-3012-mac.github.io/Echoes-of-an-Unsaid-Goodbye-.pdf';
+
+// Button references
 const readBtn = document.getElementById('readBook');
 const downloadBtn = document.getElementById('downloadBook');
-const pdfFile = 'book.pdf'; // Make sure book.pdf is in the same folder
 
 // Open PDF in new tab
 readBtn.addEventListener('click', () => {
-  window.open(pdfFile, '_blank');
+  window.open(pdfFile, '_blank'); // Opens the PDF online
 });
 
 // Download PDF
 downloadBtn.addEventListener('click', () => {
   const link = document.createElement('a');
   link.href = pdfFile;
-  link.download = 'Echoes_of_an_Unsaid_Goodbye.pdf';
+  link.download = 'Echoes-of-an-Unsaid-Goodbye.pdf'; // Name it when downloaded
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
 });
-
